d7c9984888fb4921acd3a8660f401bdc5214308e
RAJMAL PANKAJ <112754581+Rajmalpankaj786@users.noreply.github.com>
2024-05-07 08:06:53 +0530

    Revert "Day3"

--- inuput_taking/logical.py
+++
@@ -1,8 +0,0 @@
-#find odd , even and even-odd
-a,b = map(int, input().split())
-if(a%2==0 and b%2==0):
-    print("Even")
-elif(a%2 != 0 and b%2 != 0):
-    print("Odd")
-else:
-    print("Even-Odd")
